161e517f5a8c560326b764d168c3379c4929995f
comdori-wj <02145s1@gmail.com>
2024-02-08 17:39:37 +0900

    [Feat] 미리보기 화면 추가

---
+++ MagicScanner/MagicScanner/Controllers/PreViewController.swift
@@ -0,0 +1,31 @@
+//
+//  PreViewController.swift
+//  MagicScanner
+//
+//  Created by Wonji Ha on 2/8/24.
+//
+
+import UIKit
+
+final class PreViewController: UIViewController {
+    private let cameraManager = CameraManager()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .black
+        
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
